4edbb64182dc87275d1b1f82015e12a933ebccca
Iman Mousaei <imanmousaei@sotoon.ir>
2022-03-19 14:13:02 +0330

    Send reminder to telegram

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
+import telegram
 
 from consts import LIFETIME
+from telegram_config import BOT_TOKEN, CHAT_ID
 
 
 def load_config():
@@ -21,9 +23,24 @@ def check_car():
     should_check = dict(sorted(should_check.items(), key=lambda item: item[1]))
     json.dump(should_check, open('should_check.json', 'w'), indent=4)
 
+    return should_check
+
+
+def get_formatted(should_check):
+    from prettytable import PrettyTable
+    table = PrettyTable(['Name', 'KM'])
+
     for key in should_check:
-        print(f"You should check {key} at {should_check[key]} km")
+        row = [key, should_check[key]]
+        table.add_row(row)
+    
+    return f'<pre>{table}</pre>'
+
+def telegram_reminder(should_check):
+    bot = telegram.Bot(token=BOT_TOKEN)
+    bot.sendMessage(chat_id=CHAT_ID, text=get_formatted(should_check), parse_mode=telegram.ParseMode.HTML)
 
 
 if __name__ == '__main__':
-    check_car()
+    should_check = check_car()
+    telegram_reminder(should_check)
